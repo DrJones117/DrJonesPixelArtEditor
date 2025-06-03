e6b976c5991a0f015bb9ec62921d7e8e09a5ef57
Judah Kahler <judah.kahler@gmail.com>
2025-06-03 11:37:09 -0500

    Pixel Art project start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DrJonesPixelArtEditor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
